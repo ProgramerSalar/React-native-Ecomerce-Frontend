efcb1c9995c7c150fe2696f3144afea047dfdab4
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-21 13:53:01 +0530

    Header components

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { defaultStyle } from '../styles/styles'
+import Header from '../components/Header'
+
 
 export const Home = () => {
   return (
     <View style={defaultStyle}>
       {/* header */}
+      <Header/>
 
       <View>
         <Text>Our</Text>
