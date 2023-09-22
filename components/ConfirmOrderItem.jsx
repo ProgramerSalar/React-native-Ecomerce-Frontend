856bb41f05601ac57fb7d2b2c53dd8e68a97c82d
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-22 20:40:07 +0530

    ComfirmOrderItem page

--- components/ConfirmOrderItem.jsx
+++ components/ConfirmOrderItem.jsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-const ConfirmOrderItem = () => {
-  return (
-    <View>
-      <Text>ConfirmOrderItem</Text>
-    </View>
-  )
+const ConfirmOrderItem = ({ price, quantity, image, name }) => {
+    return (
+        <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            margin: 10
+        }}>
+            <Image
+            source={{
+                uri:image,
+            }}
+            style={{
+                width:50,
+                height:50,
+                resizeMode:'contain'
+            }}
+            />
+
+            <Text>{name}</Text>
+            <View style={{
+                flexDirection:'row'
+            }}>
+                <Text>{quantity}</Text>
+                <Text style={{
+                    marginHorizontal:10
+                }}>X</Text>
+                <Text>${price}</Text>
+            </View>
+            
+
+        </View>
+    )
 }
 
 export default ConfirmOrderItem
