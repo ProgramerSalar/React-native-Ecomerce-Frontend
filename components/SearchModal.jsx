f2dab5ed269977cd653af692089eff88452a3cdb
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-22 11:23:09 +0530

    search component

---
+++ components/SearchModal.jsx
@@ -0,0 +1,141 @@
+import { Image, View, Text, Platform, StatusBar, SafeAreaView, ScrollView, TouchableOpacity, BackHandler } from 'react-native'
+import React, { useEffect, useReducer } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { colors } from '../styles/styles'
+import { Headline, Searchbar } from 'react-native-paper'
+
+
+
+
+
+
+
+const SearchModal = ({
+    searchQuery,
+    setActiveSearch,
+    setSearchQuery,
+    products = [],
+}) => {
+
+    const navigate = useNavigation()
+
+
+    const backAction = () => {
+        setSearchQuery("")  // this code are empty the search input when you are going to another things, you search again 
+        setActiveSearch(false)     // if you get not search search is off then if search input the True means [default search is false] 
+        return true;    // return for true 
+    }
+
+
+    useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", backAction)
+
+        return () => {
+            BackHandler.removeEventListener("hardwareBackPress", backAction)
+        }
+    }, [] )
+
+
+    return (
+        <View
+            style={{
+                width: "100%",
+                height: "100%",
+                position: 'absolute',
+                top: 0,
+                zIndex: 100,
+                backgroundColor: colors.color2,
+                padding: 35,
+                paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+            }}
+        >
+
+
+            <SafeAreaView>
+                <Searchbar placeholder='Search....' onChangeText={(query) => setSearchQuery(query)}
+                    value={searchQuery}
+                    style={{ marginTop: 20 }}
+                />
+                <ScrollView>
+                    <View style={{
+                        paddingVertical: 40,
+                        paddingHorizontal: 10
+                    }}>
+
+                        {
+                            products.map((i) => (
+                                <SearchItem
+                                    key={i._id}
+                                    imgSrc={i.images[0]?.url}
+                                    name={i.name}
+                                    price={i.price}
+                                    handler={
+                                        () => navigate.navigate("productdetails", { id: i._id })
+                                    }
+                                />
+                            ))
+                        }
+
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+
+
+
+
+        </View>
+    )
+}
+
+
+const SearchItem = ({ price, name, imgSrc, handler }) => (
+    <TouchableOpacity onPress={handler}>
+        <View style={{
+            padding: 20,
+            borderRadius: 10,
+            backgroundColor: colors.color2,
+            elevation: 5,
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            flexDirection: 'row',
+            marginVertical: 30,
+        }}>
+
+            <Image source={{
+                uri: imgSrc,
+            }}
+                style={{
+                    width: 80,
+                    height: 80,
+                    position: 'absolute',
+                    resizeMode: "contain",
+                    top: -15,
+                    left: 10,
+                    borderTopLeftRadius: 20,
+                    borderRadiusRightRadious: 20,
+                }}
+            />
+
+
+            <View
+                style={{
+                    width: "80%",
+                    paddingHorizontal: 30
+                }}>
+                <Text numberOfLines={1}>{name}</Text>
+                <Headline
+                    numberOfLines={1}
+                    style={{
+                        fontWeight: "900"
+                    }}
+                > ₹{price}</Headline>
+
+
+            </View>
+        </View>
+
+    </TouchableOpacity>
+)
+
+export default SearchModal
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,24 +1,147 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { defaultStyle } from '../styles/styles'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { colors, defaultStyle } from '../styles/styles'
 import Header from '../components/Header'
+import { Avatar, Button } from 'react-native-paper'
+import SearchModal from '../components/SearchModal'
 
+const categories = [
+  { category: "footbal", _id: "1" },
+  { category: "ball", _id: "2" },
+  { category: "baT", _id: "3" },
+  { category: "copy", _id: "4" },
+  { category: "book", _id: "5" },
+  { category: "bag", _id: "6" },
+  { category: "mobile", _id: "7" },
+]
+
+
+const products = [{
+  price:435,
+  name:"manish",
+  _id:"1",
+  images:[
+    {
+      url:"https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
+
+    }
+  ]
+
+}]
 
 export const Home = () => {
-  return (
-    <View style={defaultStyle}>
-      {/* header */}
-      <Header/>
 
-      <View>
-        <Text>Our</Text>
-        <Text>Products</Text>
+
+
+
+
+
+  const [category, setCategory] = useState("")
+  const [activeSearch, setActiveSearch] = useState(false)   // search component 
+  const [searchQuery, setSearchQuery] = useState("")
+
+
+
+
+
+
+const categoryButtonHandler = (id) => {
+  // console.log(id)
+  setCategory(id)
+}
+
+// console.log(category)
+  return (
+    <>
+
+{
+  activeSearch && (
+    <SearchModal 
+    searchQuery={searchQuery}
+    setSearchQuery={setSearchQuery}
+    setActiveSearch={setActiveSearch}
+    products={products}
+    />
+  )
+}
+   
+
+
+
+
+    <View style={defaultStyle}>
+      <Header />
+
+      {/* Heading row  */}
+      <View style={{
+        paddingTop: 70,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        {/* header */}
+        <View >
+          <Text style={{ fontSize: 25 }}>Our</Text>
+          <Text style={{ fontSize: 25 }}>Products</Text>
+        </View>
+
+        {/* search Bar  */}
+        <View>
+          <TouchableOpacity onPress={() => setActiveSearch((prev) => !prev)}>
+            <Avatar.Icon icon={"magnify"} color={"gray"} style={{ backgroundColor: colors.color2, elevation: 12 }} size={50} />
+          </TouchableOpacity>
+        </View>
+
+
+
       </View>
+
+
+      {/* categories  */}
+      <View style={{
+        flexDirection: 'row',
+        height: 80
+      }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ alignItems: 'center' }}>
+          {
+            categories.map((item, index) => (
+              <Button
+                key={item._id}
+                style={{
+                  backgroundColor: category===item._id ? colors.color1 : colors.color5,
+                  borderRadius: 100,
+                  margin: 5
+                }}
+                onPress={() => categoryButtonHandler(item._id)}>
+                <Text style={{
+                  fontSize: 12,
+                  color: category === item._id ? colors.color2 : "gray"
+                }}>
+
+                </Text>
+                {item.category}
+              </Button>
+            ))
+          }
+        </ScrollView>
+
+      </View>
+
+
+   
+
+
+
+
 
 
 
 
     </View>
+    </>
+
+    
+    
   )
 }
 
