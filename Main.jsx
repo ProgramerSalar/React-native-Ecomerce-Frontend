83e4c39481a07e18ff1ca0caea0dd2c85ff0c5ed
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-23 23:06:12 +0530

    Building Order Screen

--- Main.jsx
+++ Main.jsx
@@ -15,6 +15,7 @@ import SignUp from './screens/SignUp';
 import Profile from './screens/Profile';
 import UpdateProfile from './screens/UpdateProfile';
 import ChangePassword from './screens/ChangePassword';
+import Orders from './screens/Orders';
 
 const Stack = createNativeStackNavigator()
 
@@ -40,6 +41,7 @@ function Main() {
                 <Stack.Screen name='profile' component={Profile}></Stack.Screen>
                 <Stack.Screen name='updateprofile' component={UpdateProfile}></Stack.Screen>
                 <Stack.Screen name='changepassword' component={ChangePassword}></Stack.Screen>
+                <Stack.Screen name='orders' component={Orders}></Stack.Screen>
 
                 {/* Password Reseting Route  */}
                 <Stack.Screen name='forgetpassword' component={ForgetPassword}></Stack.Screen>
