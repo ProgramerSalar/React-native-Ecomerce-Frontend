399449a3253466cdf781a050603f50a283563ad6
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-24 20:18:27 +0530

    Setting up Backend

---
+++ README.md
@@ -0,0 +1,5 @@
+- create the the project using thie command : npm init 
+
+- install the project express, mongoose, dotenv, mongodb
+
+- install nodemon 
---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { config } from "dotenv";
+
+
+config({
+    path: "./data/config.env",
+  });
+  
+
+
+  
+export const app = express ()
