43b82f7cfb26812b37cd1354e44c8cf25ae112c6
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-26 13:32:23 +0530

    Search and filter API

--- controllers/product.js
+++ controllers/product.js
@@ -7,7 +7,7 @@ import { Category } from "../models/category.js";
 
 export const getAdminProducts = asyncError(async ({ req, res, next }) => {
   // search and category query;
-  const products = await Product.find({});
+  const products = await Product.find({}).populate("category")
 
   res.status(200).json({
     success: true,
@@ -15,10 +15,17 @@ export const getAdminProducts = asyncError(async ({ req, res, next }) => {
   });
 });
 
-
 export const getAllProducts = asyncError(async ({ req, res, next }) => {
   // search and category query;
-  const products = await Product.find({});
+
+  const { keyword, category } = req.query;  
+  const products = await Product.find({
+    name: {
+      $regex: keyword ? keyword : "",
+      $options: "i",
+    },
+    category: category ? category : undefined,
+  });
 
   res.status(200).json({
     success: true,
@@ -27,7 +34,7 @@ export const getAllProducts = asyncError(async ({ req, res, next }) => {
 });
 
 export const getProductDetails = asyncError(async (req, res, next) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate("category")
 
   if (!product) return next(new ErrorHandler("Product not found", 404));
 
@@ -152,10 +159,6 @@ export const deleteProduct = asyncError(async (req, res, next) => {
   });
 });
 
-
-
-
-
 export const getAllCategories = asyncError(async (req, res, next) => {
   const categories = await Category.find({});
 
@@ -175,34 +178,21 @@ export const addCategory = asyncError(async (req, res, next) => {
   });
 });
 
-
-
 export const deleteCategory = asyncError(async (req, res, next) => {
   const category = await Category.findById(req.params.id);
-    if (!category) return next(new ErrorHandler("Category Not Found", 404));
-    const products = await Product.find({ category: category._id });
+  if (!category) return next(new ErrorHandler("Category Not Found", 404));
+  const products = await Product.find({ category: category._id });
 
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    product.category = undefined;
+    await product.save();
+  }
 
-    for (let i = 0; i < products.length; i++) {
-      const product = products[i];
-      product.category = undefined;
-      await product.save();
-    }
-
-    await category.deleteOne({ _id: req.params.id });
+  await category.deleteOne({ _id: req.params.id });
 
   res.status(200).json({
     success: true,
     message: "Category deleted Successfully",
   });
 });
-
-
-
-
-
-
-
-
-
-
