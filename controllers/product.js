55ca064457eee018383007fa21fbd77b17e0757c
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-26 09:57:16 +0530

    Upload Product and Add Images

--- controllers/product.js
+++ controllers/product.js
@@ -4,8 +4,6 @@ import ErrorHandler from "../utils/error.js";
 import { getDataUri } from "../utils/features.js";
 import cloudinary from "cloudinary";
 
-
-
 export const getAllProducts = asyncError(async ({ req, res, next }) => {
   // search and category query;
   const products = await Product.find({});
@@ -53,3 +51,47 @@ export const createProduct = asyncError(async (req, res, next) => {
     message: "Product Created Successfully",
   });
 });
+
+export const updateProduct = asyncError(async (req, res, next) => {
+  const { name, description, category, price, stock } = req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+
+  if (name) product.name = name;
+  if (description) product.description = description;
+  if (category) product.category = category;
+  if (price) product.price = price;
+  if (stock) product.stock = stock;
+
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Updated Successfully",
+  });
+});
+
+export const addProductImage = asyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+
+  if (!req.file) return next(new ErrorHandler("Please add image", 400));
+
+  const file = getDataUri(req.file);
+  const myCloud = await cloudinary.v2.uploader.upload(file.content);
+  const image = {
+    public_id: myCloud.public_id,
+    url: myCloud.secure_url,
+  };
+
+  product.images.push(image); // image are push on images folder
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Image Added Successfully",
+  });
+});
+
+
--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,27 @@
 import express from "express";
 
-import { getProductDetails, getAllProducts, createProduct } from "../controllers/product.js";
+import {
+  getProductDetails,
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  addProductImage,
+  deleteProductImage,
+} from "../controllers/product.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.get("/all", getAllProducts);
-router.route("/single/:id").get(getProductDetails);
-router.post("/new", isAuthenticated, singleUpload, createProduct)
+router
+  .route("/single/:id")
+  .get(getProductDetails)
+  .put(isAuthenticated, updateProduct);
+router.post("/new", isAuthenticated, singleUpload, createProduct);
+
+router
+  .route("/images/:id")
+  .post(isAuthenticated, singleUpload, addProductImage).delete(isAuthenticated, deleteProductImage)
 
 export default router;
